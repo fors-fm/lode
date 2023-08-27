a1391b5548c2ce229cc4491e3777588c51f2e10a
Ess <48392793+ess-m@users.noreply.github.com>
2023-08-27 17:35:57 +0200

    initial commit

---
+++ dial.js
@@ -0,0 +1,299 @@
+outlets = 1
+
+mgraphics.init()
+mgraphics.autofill = 0
+mgraphics.relative_coords = 0
+
+var style = "decay"
+
+function set_style(x) {
+	style = x
+	refresh()
+    mgraphics.redraw()
+}
+
+var value = 0
+
+function set_value(x) {
+	value = x
+	refresh()
+    mgraphics.redraw()
+}
+
+// color handling
+
+var lcdcolor = [0.993, 0.654, 0.155, 1.0]
+var inactivelcdcolor = [0.475, 0.475, 0.475, 1.0]
+var lcdbgcolor = [0.118, 0.118, 0.118, 1.0]
+var hovercolor = [
+    inactivelcdcolor[0] * 1.7,
+    inactivelcdcolor[1] * 1.7,
+    inactivelcdcolor[2] * 1.7,
+    1.0,
+]
+
+function set_lcdcolor(r, g, b, a) {
+    lcdcolor = [r, g, b, a]
+    refresh()
+    mgraphics.redraw()
+}
+
+function set_lcdbgcolor(r, g, b, a) {
+    lcdbgcolor = [r, g, b, a]
+    refresh()
+    mgraphics.redraw()
+}
+
+function set_inactivelcdcolor(r, g, b, a) {
+    inactivelcdcolor = [r, g, b, a]
+    hovercolor = [
+        inactivelcdcolor[0] * 1.7,
+        inactivelcdcolor[1] * 1.7,
+        inactivelcdcolor[2] * 1.7,
+        1,
+    ]
+    refresh()
+    mgraphics.redraw()
+}
+
+// hover transition task
+
+var hover_state = 0
+var hover_task = new Task(hover_transition, this)
+
+var repeats = 50
+var interval = 10
+
+function hover_transition() {
+	if (arguments.callee.task.iterations >= repeats) {
+		if (hover) {
+			hover_state = 1
+			mgraphics.redraw()
+		} else {
+			hover_state = 0
+			mgraphics.redraw()
+		}
+		
+		arguments.callee.task.cancel()
+	} else {
+		mgraphics.redraw()
+		
+		if (hover) {
+			hover_state = sigmoid((arguments.callee.task.iterations / repeats), -0.875)
+		} else {
+			hover_state = sigmoid(1 - (arguments.callee.task.iterations / repeats), 0.5)
+		}
+	}
+}
+
+// mousing states
+
+var hover = 0
+var hover_in = false
+
+var last_x = 0, last_y = 0
+var ratio_x = 0, ratio_y = 0
+var click_x = 0, click_y = 0
+
+function onidle(x, y, button, cmd, shift, capslock, option, ctrl) {
+	hover = 1
+	
+	if (!hover_in) {
+		hover_task.interval = interval
+		hover_task.repeat(repeats)
+		hover_task.execute()
+		
+		hover_in = true
+	}
+	
+    mgraphics.redraw()
+}
+
+function onidleout(x, y) {
+	hover = 0
+	
+	if (hover_in) {
+		hover_task.interval = interval
+		hover_task.repeat(repeats)
+		hover_task.execute()
+		
+		hover_in = false
+	}
+	
+    mgraphics.redraw()
+}
+
+function onclick(x, y, but, cmd, shift, capslock, option, ctrl) {
+	max.hidecursor()
+	
+	// calculate the UI zoom factor by getting the ratio of the original box size
+	// and the current bpatcher window location affected by the zoom factor
+	ratio_x = (patcher.wind.location[2] - patcher.wind.location[0]) / box.rect[2]
+	ratio_y = (patcher.wind.location[3] - patcher.wind.location[1]) / box.rect[3]
+	
+	// store bpatcher position which will be used to calculate the cursor delta
+	last_x = patcher.wind.location[0]
+	last_y = patcher.wind.location[1]
+	
+	// get and scale relative cursor position for resetting to initial click location
+	click_x = Math.round(patcher.wind.location[0] + x * ratio_x)
+	click_y = Math.round(patcher.wind.location[1] + y * ratio_y)
+	
+	// move cursor to expected position for delta calculation
+	max.pupdate(last_x, last_y);
+}
+
+function ondblclick(x, y) {
+    value = 0
+
+    outlet(0, value)
+    mgraphics.redraw()
+}
+
+function ondrag(x, y, but, cmd, shift, capslock, option, ctrl) {
+    var dy = 0,
+        d2y = 0
+    var dx = 0,
+        d2x = 0
+
+    var delta_x
+    var delta_y
+
+	// release cursor and reset position on mouse up
+    if (!but) {
+		max.pupdate(click_x, click_y)
+        max.showcursor()
+
+        mgraphics.redraw()
+    } else {
+		// calculate the delta between initial cursor position and after drag
+		dx = (last_x + x) - last_x
+		dy = (last_y + y) - last_y
+
+		// increase resolution if shift modifier key is held down
+        if (shift) {
+            delta_x = 0.0007874015748
+            delta_y = 0.0007874015748
+        } else {
+            delta_x = 0.007874015748
+            delta_y = 0.007874015748
+        }
+		
+		d2x = dx * delta_x
+		d2y = dy * delta_y
+		
+		// add scaled delta to the output value and clamp to parameter range
+		value = clamp(value - d2y, 0, 1)
+
+		// reset cursor position if it has changed, we do this to be able to 
+		// calculate the delta from the same exact position on every drag
+		if (dy > 0 || dy < 0 || dx > 0 || dx < 0) {
+			max.pupdate(last_x, last_y)
+		}
+		
+        mgraphics.redraw()
+        outlet(0, value)
+    }
+}
+
+function paint() {
+    mgraphics.set_line_cap("round")
+    mgraphics.set_line_join("round")
+    mgraphics.set_line_width(2)
+
+    with (mgraphics) {
+		switch (style) {
+			case "dial":
+				set_source_rgba(inactivelcdcolor)
+				
+				arc(18, 18, 12, 50 * Math.PI / 180, 130 * Math.PI / 180)
+				stroke()
+			
+				get_lcdcolor(hover_state)
+				
+				arc(18, 18, 12, 130 * Math.PI / 180, 410 * Math.PI / 180)
+				stroke()
+		
+				line_to_angle(18, 18, -230 + value * 280, 0, 7)
+				stroke()
+				break
+				
+			case "slider":
+				set_source_rgba(inactivelcdcolor)
+				
+				for (i = 0; i < 5; ++i) {
+					rectangle(14, 7 + i * 5, 8, 2)
+					fill()
+				}
+				
+				get_lcdcolor(hover_state)
+			
+				rectangle_rounded(11, 24 - value * 20, 14, 8, 4, 4)
+				fill()
+		
+				set_source_rgba(lcdbgcolor)
+				rectangle(14, 27 - value * 20, 8, 2)
+				fill()
+				break
+				
+			case "decay":
+				set_source_rgba(inactivelcdcolor)
+				
+				move_to(7 + value * 23, 29)
+				line_to(30, 29)
+				stroke()
+				
+				move_to(6, 29)
+				rel_line_to(0, -21)
+				stroke()
+			
+				get_lcdcolor(hover_state)
+			
+				var x_point = 7 + value * 23
+			
+				move_to(6, 7)
+				curve_to(x_point - (20 * value) + 0.01, 29, x_point, 29, x_point, 29)
+				stroke()
+				break
+		}
+    }
+}
+
+// utility functions
+
+function get_lcdcolor(state) {
+	mgraphics.set_source_rgba(
+		mix(lcdcolor[0], hovercolor[0], state),
+		mix(lcdcolor[1], hovercolor[1], state),
+		mix(lcdcolor[2], hovercolor[2], state),
+		mix(lcdcolor[3], hovercolor[3], state)
+	)
+}
+
+function sigmoid(x, y) {
+	return (x - x * y) / (y - Math.abs(x) * 2 * y + 1)
+}
+
+function mix(x, y, a) {
+    return x + a * (y - x)
+}
+
+function clamp(num, min, max) {
+    return num <= min ? min : num >= max ? max : num
+}
+
+function line_to_angle(x, y, degrees, offset, length) {
+  var angle = degrees * Math.PI / 180
+  var cos_angle = Math.cos(angle)
+  var sin_angle = Math.sin(angle)
+
+  var x_origin = cos_angle * offset + x 
+  var y_origin = sin_angle * offset + y
+  var x_destination = cos_angle * length + x_origin
+  var y_destination = sin_angle * length + y_origin
+
+  with (mgraphics) {  
+    	move_to(x_origin, y_origin)
+    	line_to(x_destination, y_destination)
+    }
+}
---
+++ dial.maxpat
@@ -0,0 +1,109 @@
+{
+	"patcher" : 	{
+		"fileversion" : 1,
+		"appversion" : 		{
+			"major" : 8,
+			"minor" : 5,
+			"revision" : 5,
+			"architecture" : "x64",
+			"modernui" : 1
+		}
+,
+		"classnamespace" : "box",
+		"rect" : [ 517.0, 393.0, 640.0, 480.0 ],
+		"bglocked" : 0,
+		"openinpresentation" : 1,
+		"default_fontsize" : 12.0,
+		"default_fontface" : 0,
+		"default_fontname" : "Arial",
+		"gridonopen" : 1,
+		"gridsize" : [ 15.0, 15.0 ],
+		"gridsnaponopen" : 1,
+		"objectsnaponopen" : 1,
+		"statusbarvisible" : 2,
+		"toolbarvisible" : 1,
+		"lefttoolbarpinned" : 0,
+		"toptoolbarpinned" : 0,
+		"righttoolbarpinned" : 0,
+		"bottomtoolbarpinned" : 0,
+		"toolbars_unpinned_last_save" : 0,
+		"tallnewobj" : 0,
+		"boxanimatetime" : 200,
+		"enablehscroll" : 1,
+		"enablevscroll" : 1,
+		"devicewidth" : 0.0,
+		"description" : "",
+		"digest" : "",
+		"tags" : "",
+		"style" : "",
+		"subpatcher_template" : "",
+		"assistshowspatchername" : 0,
+		"boxes" : [ 			{
+				"box" : 				{
+					"comment" : "",
+					"id" : "obj-3",
+					"index" : 0,
+					"maxclass" : "inlet",
+					"numinlets" : 0,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 30.0, 30.0, 30.0, 30.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"comment" : "",
+					"id" : "obj-2",
+					"index" : 0,
+					"maxclass" : "outlet",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 30.0, 150.0, 30.0, 30.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"border" : 0,
+					"filename" : "dial.js",
+					"id" : "obj-1",
+					"maxclass" : "jsui",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"parameter_enable" : 0,
+					"patching_rect" : [ 30.0, 90.0, 36.0, 36.0 ],
+					"presentation" : 1,
+					"presentation_rect" : [ 0.0, 0.0, 36.0, 36.0 ]
+				}
+
+			}
+ ],
+		"lines" : [ 			{
+				"patchline" : 				{
+					"destination" : [ "obj-2", 0 ],
+					"source" : [ "obj-1", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"source" : [ "obj-3", 0 ]
+				}
+
+			}
+ ],
+		"dependency_cache" : [ 			{
+				"name" : "dial.js",
+				"bootpath" : "~/Library/CloudStorage/Dropbox/Max/cursortext",
+				"patcherrelativepath" : ".",
+				"type" : "TEXT",
+				"implicit" : 1
+			}
+ ],
+		"autosave" : 0
+	}
+
+}
