8d463a3d20e7474565b8e17a86dc4ff7dd141109
Ess <48392793+ess-m@users.noreply.github.com>
2023-08-29 11:48:18 +0200

    fix: unroll all names

--- set-name.js
+++ set-name.js
@@ -3,7 +3,7 @@ var parameters = new Dict("---parameters")
 function bang() {
 	var param_title = ""
 	
-	for (i = 0; i < 1; ++i) {
+	for (i = 0; i < 8; ++i) {
 		param_title = parameters.get("param_" + i + "::title")
 		this.patcher.getnamed("---text" + i).message('set', param_title)
 	}
